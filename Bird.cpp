4529af8d73d482a2a69168019091fce365bdb8dc
23021584-NguyenVanHung-UET <23021584@vnu.edu.vn>
2024-05-14 01:18:46 +0700

    BTL

---
+++ Bird.cpp
@@ -0,0 +1,137 @@
+#include "Bird.h"
+#include "Pipe.h"
+#include <vector>;
+
+position bird_pipe;
+
+void bird::render()
+{
+	Render(pos_bird.x, pos_bird.y);
+	//cout << rect_pipe.x << " " << rect_pipe.y << " " << rect_pipe.w << " " << rect_pipe.h << endl;
+	//cout << rect_bird.x << " " << rect_bird.y << " " << rect_bird.w << " " << rect_bird.h << endl;
+}
+
+bool bird::init()
+{
+	pos_bird.getPos(75, SCREEN_HEIGHT / 2 - 10);
+	string path1 = "img/yellowbird1.png";
+	string path2 = "img/yellowbird2.png";
+	string path3 = "img/yellowbird3.png";
+	
+	if (isNULL())
+	{
+	animation_timer++;
+	if (animation_timer < 16)
+		{
+			if (CreatTexture(path1.c_str(), 1))
+			{
+				return true;
+			}	
+		}
+	if (animation_timer >= 16 && animation_timer <= 32)
+		{
+		animation_timer++;
+			if (CreatTexture(path2.c_str(), 1))
+			{
+				return true;
+			}
+		}
+	if (animation_timer > 32 && animation_timer <= 48)
+		{
+		animation_timer++;
+			if (CreatTexture(path3.c_str(), 1))
+			{
+				return true;
+			}
+		}
+		if (animation_timer > 48)
+		{
+			animation_timer = 0;
+		}
+	}
+	return false;
+}
+
+
+
+void bird::gravity()
+{
+	rect_bird.x = 75;
+	rect_bird.w = rect_bird.x + 10;
+	if (jump_state())
+	{
+		a = a + 0.035;
+		jump_height += g;
+		y0 += (int)(g + 2 * a + jump_height);
+		pos_bird.y = y0;
+		rect_bird.y = pos_bird.y;
+		rect_bird.h = rect_bird.y+ 10;
+		if (jump_height > 0)
+		{
+			inJump = false;
+			jump_height -= 7;
+		}
+	}
+	else
+	{
+		a += 0.035;
+		y0 += (int)(g + 2 * a);
+		pos_bird.y = y0;
+	}
+	//cout << pos_bird.x << " " << pos_bird.y << " ";
+	//cout << check_collision() << endl;
+}
+
+bool bird::check_collision()
+{
+	if (pos_bird.y >=460)
+	{
+		return true;
+	}
+	
+	
+	rect_pipe.x = pos_pipe[i].x;
+	rect_pipe.w = rect_pipe.x + 60;
+	rect_pipe.y = pos_pipe[i].y;
+	rect_pipe.h = rect_pipe.y + 373;
+	if (rect_bird.w >= rect_pipe.x && rect_bird.x <= rect_pipe.w && rect_bird.h <= rect_pipe.h) return true;
+	if (rect_bird.w >= rect_pipe.x && rect_bird.x <= rect_pipe.w && rect_bird.y >= rect_pipe.h + PIPE_SPACE) return true;
+	if (rect_bird.w >= rect_pipe.x && rect_bird.x<=rect_pipe.w && ((rect_bird.y <= rect_pipe.h && rect_bird.h >= rect_pipe.h)||
+		(rect_bird.y<=rect_pipe.w+PIPE_SPACE && rect_bird.h >= rect_pipe.w + PIPE_SPACE)))
+	{
+		return true;
+	}
+	else
+	{
+		i = (i + 1) % 4;
+		score++;
+	}
+	
+
+	if (pos_bird.y <= 0) return true;
+	return false;
+}
+
+void bird::jump()
+{
+	if (jump_timer - last_jump > 180)
+	{
+		a = 0;
+		inJump = true;
+		last_jump = jump_timer;
+	}
+	else
+	{
+		gravity();
+	}
+}
+
+void bird::get_jump_time()
+{
+	jump_timer = SDL_GetTicks();
+}
+
+bool bird::jump_state()
+{
+	return inJump;
+}
--- Bird.h
+++ Bird.h
@@ -3,9 +3,36 @@
 #define BIRD_H_
 
 #include "TextureManager.h"
+#include "Pipe.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static position pos_bird;
 
 class bird :TextureManager
 {
+private:
+	double g = 0.2;
+	int i = 0;
+	int y0 = 256;
+	double a = 0;
+	bool inJump = false;
+	double jump_height = -7;
+	double jump_timer;
+	double last_jump = 0;
+	int animation_timer;
+	SDL_Rect rect_bird;
+	SDL_Rect rect_pipe;
+public:
+	void gravity();
+	void get_jump_time();
+	void jump();
+	bool jump_state();
+	void render();
+	bool init(); 
+	bool check_collision();
 
 };
 
